71589a1ed3b9d64c5d7cac82fb602f4339fda96c
gta191977649 <episodes27@gmail.com>
2019-03-09 19:05:31 +1100

    0.1

---
+++ effect.js
@@ -0,0 +1,17 @@
+/*
+    处理页面各种特效
+    (C) Project Sparrow
+*/
+(function ($) {
+    'use strict';
+    var $window = $(window);
+    $window.on('scroll', function () {
+        if ($window.scrollTop() > 48) {
+            $('.header_area').addClass('sticky slideInDown');
+        } else {
+            $('.header_area').removeClass('sticky slideInDown');
+        }
+    });
+
+
+})(jQuery);
